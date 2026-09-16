dd9e67ef8418d7a298a0552e423d2dd7b1f8263d
Kar0r404 <mahlarekarabo.702@gmail.com>
2026-09-16 08:23:45 +0200

    the repository structure defined

--- ingestion-service/src/main/java/co/wethinkcode/logisticsconnect/repository/IngestionRepository.java
+++ ingestion-service/src/main/java/co/wethinkcode/logisticsconnect/repository/IngestionRepository.java
@@ -1,6 +1,48 @@
 package co.wethinkcode.logisticsconnect.repository;
 
+import co.wethinkcode.logisticsconnect.model.dto.IngestionRequest;
+import co.wethinkcode.logisticsconnect.model.dto.IngestionResponse;
+import co.wethinkcode.logisticsconnect.model.entity.Ingestion;
+
+import java.sql.PreparedStatement;
+import java.util.List;
+
 public class IngestionRepository {
 
+    public void save(Ingestion ingestion) {
+        String query = """
+                INSERT INTO ingestion(hub_id, province, sorting_center, is_active)
+                VALUES(?, ?, ?, ?)
+                """;
+        try (PreparedStatement statement = connection.PreparedStatement(query)) {
+            statement.setString(1, ingestion.getHubId());
+            statement.setString(2, ingestion.getProvince());
+            statement.setString(3, ingestion.getSortingCenter());
+            statement.setBoolean(4, ingestion.isActive());
+
+            statement.executeUpdate();
+        }
+
+    }
+
+    public IngestionResponse getByHubId(String hubId) {
+        return null;
+    }
+
+    public List<IngestionResponse> getByProvince(String province) {
+        return null;
+    }
+
+    public List<IngestionResponse> getBySortingCenter(String sortingCenter) {
+        return null;
+    }
+
+    public List<IngestionResponse> getAllActive() {
+        return null;
+    }
+
+    public List<IngestionResponse> getAll() {
+        return null;
+    }
 
 }
